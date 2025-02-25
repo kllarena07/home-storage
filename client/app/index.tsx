5b6cbc77ec6b34330c1bd6026075df2a992ed705
Kieran Llarena <baymax709@gmail.com>
2025-02-25 02:03:28 -0500

    feat(client): user input for server URL

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,19 +1,30 @@
-import { Alert, Pressable, Text, View } from "react-native";
+import { Alert, Pressable, Text, TextInput, View } from "react-native";
+import { useState } from "react";
 
 export default function Index() {
+  const [serverURL, setServerURL] = useState<string>("http://localhost:3000");
+
   const handlePress = async () => {
     console.log("Sending request...");
     try {
       const controller = new AbortController();
       const timeoutId = setTimeout(() => controller.abort(), 2000);
-      const response = await fetch("https://macbookair.tail05fcc.ts.net/", {
+
+      const asURL = new URL(serverURL);
+
+      const response = await fetch(asURL, {
         signal: controller.signal,
       });
+
       clearTimeout(timeoutId);
+
       const text = await response.text();
       Alert.alert(text);
     } catch (error) {
-      Alert.alert("Error", error.message);
+      Alert.alert(
+        "Error",
+        error instanceof Error ? error.message : "An unknown error occurred"
+      );
     }
   };
 
@@ -25,6 +36,19 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+      <TextInput
+        value={serverURL}
+        onChangeText={(text) => {
+          setServerURL(text);
+        }}
+        style={{
+          borderWidth: 1,
+          padding: 8,
+          width: "80%",
+          marginBottom: 10,
+        }}
+        placeholder="Enter server URL"
+      />
       <Pressable
         onPress={handlePress}
         style={{
