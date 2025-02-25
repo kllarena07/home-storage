f76124ba62c6bd7b06c755443921cdff0d3dc287
Kieran Llarena <baymax709@gmail.com>
2025-02-25 01:20:14 -0500

    chore: server dir .gitignore

---
+++ server/server.js
@@ -0,0 +1,17 @@
+// Install Express and CORS by running: npm install express cors
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('hello world');
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
