48372ef41c5f078440803c1b17e9430b68b25d66
Kieran Llarena <baymax709@gmail.com>
2025-02-25 01:48:59 -0500

    feat(client): request send, print message, and timeout after 2 sec

--- client/app/index.tsx
+++ client/app/index.tsx
@@ -1,6 +1,22 @@
-import { Text, View } from "react-native";
+import { Alert, Pressable, Text, View } from "react-native";
 
 export default function Index() {
+  const handlePress = async () => {
+    console.log("Sending request...");
+    try {
+      const controller = new AbortController();
+      const timeoutId = setTimeout(() => controller.abort(), 2000);
+      const response = await fetch("https://macbookair.tail05fcc.ts.net/", {
+        signal: controller.signal,
+      });
+      clearTimeout(timeoutId);
+      const text = await response.text();
+      Alert.alert(text);
+    } catch (error) {
+      Alert.alert("Error", error.message);
+    }
+  };
+
   return (
     <View
       style={{
@@ -9,7 +25,22 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Pressable
+        onPress={handlePress}
+        style={{
+          backgroundColor: "red",
+          padding: 10,
+        }}
+      >
+        <Text
+          style={{
+            color: "white",
+            fontWeight: "bold",
+          }}
+        >
+          Fetch
+        </Text>
+      </Pressable>
     </View>
   );
 }
